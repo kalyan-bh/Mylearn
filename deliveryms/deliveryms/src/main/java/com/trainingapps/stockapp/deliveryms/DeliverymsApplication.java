17cd26e50f97a33e135a1ad7e377f1d84bde0962
kalyan <saikalyanbhupathi6@gmail.com>
2021-07-27 10:29:02 +0530

    added git ignore file and deliveryms app

---
+++ deliveryms/deliveryms/src/main/java/com/trainingapps/stockapp/deliveryms/DeliverymsApplication.java
@@ -0,0 +1,13 @@
+package com.trainingapps.stockapp.deliveryms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverymsApplication.class, args);
+	}
+
+}
---
+++ deliveryms/deliveryms/src/test/java/com/trainingapps/stockapp/deliveryms/DeliverymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingapps.stockapp.deliveryms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
